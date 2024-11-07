ffa6370e7148dfe1d041a58a3b8329539460fb54
Nitya-Prakash <nityaprakashsahoo677@gmail.com>
2024-11-08 01:01:32 +0530

    Project Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='max-w-screen-lg mx-auto py-5 flex items-center justify-between border-b-[1px] border-zinc-700'>
             <div className="nleft flex items-center">
-                <a href="#" className='z-10 relative'>
+                <a href="https://refokus-puce.vercel.app/" className='z-10 relative'>
                     <img src="https://web.archive.org/web/20240216094729im_/https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
                 </a>
                 <div className='links flex items-center gap-10 ml-20'>
