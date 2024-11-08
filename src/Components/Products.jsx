5e1e10e92ce3467d786da2240a905984d6a1ec66
Nitya-Prakash <nityaprakashsahoo677@gmail.com>
2024-11-08 19:22:09 +0530

    Again fixing the video on hover animation

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import Product from './Product'
 import { motion } from 'framer-motion';
+import arqitelVideo from '../Assets/arqitel-43.webm'
+import ttrVideo from '../Assets/ttr-43.webm'
+import rainfallVideo from '../Assets/rainfall-43.webm'
+import yahooVideo from '../Assets/yahoo-43.webm'
+import yirVideo from '../Assets/yir2022-43.webm'
 
 const Products = () => {
     const products = [
@@ -22,19 +27,19 @@ const Products = () => {
             <div className='w-full h-full absolute top-0 pointer-events-none'>
                 <motion.div initial={{ y: pos, x: "-50%" }} animate={{ y: pos + 'rem' }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .6 }} className='window w-[24rem] h-[16rem] absolute left-[45%] overflow-hidden rounded-xl'>
                     <motion.div transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} animate={{ y: -pos + 'rem' }} className='w-full h-full rounded-xl'>
-                        <video autoPlay muted loop src="../Assets/arqitel-43.webm" className='w-full h-full object-cover'></video>
+                        <video autoPlay muted loop src={arqitelVideo} className='w-full h-full object-cover'></video>
                     </motion.div>
                     <motion.div transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} animate={{ y: -pos + 'rem' }} className='w-full h-full bg-green-950 rounded-xl'>
-                        <video autoPlay muted loop src="../Assets/ttr-43.webm" className='w-full h-full object-cover'></video>
+                        <video autoPlay muted loop src={ttrVideo} className='w-full h-full object-cover'></video>
                     </motion.div>
                     <motion.div transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} animate={{ y: -pos + 'rem' }} className='w-full h-full bg-red-950 rounded-xl'>
-                        <video autoPlay muted loop src="../Assets/yir2022-43.webm" className='w-full h-full object-cover'></video>
+                        <video autoPlay muted loop src={yirVideo} className='w-full h-full object-cover'></video>
                     </motion.div>
                     <motion.div transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} animate={{ y: -pos + 'rem' }} className='w-full h-full bg-blue-950 rounded-xl'>
-                        <video autoPlay muted loop src="../Assets/yahoo-43.webm" className='w-full h-full object-cover'></video>
+                        <video autoPlay muted loop src={yahooVideo} className='w-full h-full object-cover'></video>
                     </motion.div>
                     <motion.div transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} animate={{ y: -pos + 'rem' }} className='w-full h-full bg-yellow-950 rounded-xl'>
-                        <video autoPlay muted loop src="../Assets/rainfall-43.webm" className='w-full h-full object-cover'></video>
+                        <video autoPlay muted loop src={rainfallVideo} className='w-full h-full object-cover'></video>
                     </motion.div>
                 </motion.div>
             </div>
